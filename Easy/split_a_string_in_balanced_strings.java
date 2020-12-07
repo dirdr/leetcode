a357432023251d1df6fcab71e1525a3917f84b0a
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2020-12-07 13:11:34 +0100

    Create split_a_string_in_balanced_strings.java
    Runtime: 0 ms, faster than 100.00% of Java online submissions for Split a String in Balanced Strings.
    Memory Usage: 36.8 MB, less than 55.73% of Java online submissions for Split a String in Balanced Strings.

---
+++ Easy/split_a_string_in_balanced_strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int countR = 0;
+        int countL = 0;
+        int countBalancedString = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == 'R')
+                countR++;
+            else
+                countL++;
+            if(countR == countL)
+                countBalancedString++;
+            
+        }
+      return countBalancedString;
+    }
+}
