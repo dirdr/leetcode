1a99e1fbc8751d4a21e4a24b2cb4f50ac3b33c35
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2020-12-07 14:11:07 +0100

    Create design_parking_system.java
    Runtime: 6 ms, faster than 99.67% of Java online submissions for Design Parking System.
    Memory Usage: 39.7 MB, less than 42.03% of Java online submissions for Design Parking System.

---
+++ Easy/design_parking_system.java
@@ -0,0 +1,22 @@
+class ParkingSystem {
+    
+    int[] parking;
+    
+
+    public ParkingSystem(int big, int medium, int small) {
+        parking = new int[3];
+        parking[0] = big;
+        parking[1] = medium;
+        parking[2] = small;
+    }
+    
+    public boolean addCar(int carType) {
+         return parking[carType-1]-->0;
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * boolean param_1 = obj.addCar(carType);
+ */
