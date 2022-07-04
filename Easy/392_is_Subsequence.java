513419db33ed79ef9627cab980730903417ddb98
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2022-07-04 14:28:28 +0200

    Update 392_is_Subsequence.java
    Runtime: 2 ms, faster than 46.59% of Java online submissions for Is Subsequence.
    Memory Usage: 41.6 MB, less than 77.19% of Java online submissions for Is Subsequence.

--- Easy/392_is_Subsequence.java
+++ Easy/392_is_Subsequence.java
@@ -1,27 +1,22 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-  
-        
+
         if (s.length() == 0) return true;
-        
-        StringBuilder sb = new StringBuilder();
-        int looking_for = 0;
-        
+        int index = 0;
+
         for (int i = 0; i < t.length(); i++) {
-            char lfc = s.charAt(looking_for);
-            if (t.charAt(i) == lfc) {
-                sb.append(t.charAt(i));
-                looking_for++;
-            }
-            if (sb.length() == s.length()) return true;
-        }
-        
-        System.out.println(sb.toString());
-        return false;        
+           char curr = t.charAt(i);
+           if (s.charAt(index) == curr) {
+               index++;
+           }
+           if (index == s.length())  return true;
+        } 
+
+        return false; 
     }
 }
 
 /**
-* Runtime: 1 ms, faster than 87.30% of Java online submissions for Is Subsequence.
-* Memory Usage: 39.9 MB, less than 42.58% of Java online submissions for Is Subsequence.
+Runtime: 2 ms, faster than 46.59% of Java online submissions for Is Subsequence.
+Memory Usage: 41.6 MB, less than 77.19% of Java online submissions for Is Subsequence.
 */
