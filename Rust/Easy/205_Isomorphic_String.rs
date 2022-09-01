5fbde4a1b59530ddd4830aeb4b6e3b171488f7e9
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2022-09-01 18:22:06 +0200

    Update 205_Isomorphic_String.rs
    Reducing to use only one hash set

--- Rust/Easy/205_Isomorphic_String.rs
+++ Rust/Easy/205_Isomorphic_String.rs
@@ -1,21 +1,21 @@
-use std::collections::hash_map::Entry;
-use std::collections::HashMap;
-use std::collections::HashSet;
+use std::collections:: {HashMap, HashSet, hash_map::Entry};
 
 impl Solution {
     pub fn is_isomorphic(s: String, t: String) -> bool {
         let mut map: HashMap<char, char> = HashMap::new();
-        let mut card_t: HashSet<char> = HashSet::new();
-        let mut card_s: HashSet<char> = HashSet::new();
+        let mut set: HashSet<char> = HashSet::new();
         
         for tup in s.as_str().chars().zip(t.as_str().chars()) {
             let (s_char, t_char) = tup;
-            card_t.insert(s_char);
-            card_s.insert(t_char);
             let entry = map.entry(s_char);
             match entry {
                 Entry::Vacant(v) => {
                     v.insert(t_char);
+                    if set.contains(&t_char) {
+                        return false;
+                    } else {
+                        set.insert(t_char);
+                    }
                 },
                 Entry::Occupied(o) => {
                     if *o.get() != t_char {
@@ -24,6 +24,6 @@ impl Solution {
                 },
             }
         }
-        card_t.len() == card_s.len()
+        true
     }
 }
