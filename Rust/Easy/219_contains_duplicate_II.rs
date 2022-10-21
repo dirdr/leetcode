ed488afc1fd09e3663c8a24bcb3f84daa111fd85
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2022-10-21 16:15:48 +0200

    Create 219_contains_duplicate_II.rs
    map

---
+++ Rust/Easy/219_contains_duplicate_II.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        for i in 0..nums.len() {
+            match (map.get(&nums[i])) {
+                Some(val) => {
+                    let diff: i32 = i as i32 - *val;
+                    if i32::abs(diff) <= k && i as i32 != *val { return true; }
+                
+                }
+                None => {}
+            }
+            map.insert(nums[i], i as i32);
+        }
+        false
+    }
+}
