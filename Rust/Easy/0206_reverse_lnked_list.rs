8e8ea1d857adf781fee9178b9e02fe262eb71479
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2023-11-14 12:08:23 +0100

    Create 0206_reverse_lnked_list.rs

---
+++ Rust/Easy/0206_reverse_lnked_list.rs
@@ -0,0 +1,28 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+//
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let (mut previous, mut current) = (None, head);
+        while let Some(mut node) = current {
+            let next = node.next;
+            node.next = previous;
+            current = next;
+            previous = Some(node)
+        }
+        previous
+    }
+}
