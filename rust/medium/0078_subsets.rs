246f7dc14e37138268674d75d569a929ab7046da
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2024-05-07 22:04:15 +0200

    Update 0078_subsets.rs

--- rust/medium/0078_subsets.rs
+++ rust/medium/0078_subsets.rs
@@ -1,23 +1,15 @@
 impl Solution {
     pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut result: Vec<Vec<i32>> = Vec::new();
-        Self::backtrack(&mut result, &mut Vec::new(), &nums[..], 0);
-        result
-    }
-
-    pub fn backtrack(
-        result: &mut Vec<Vec<i32>>,
-        temp: &mut Vec<i32>,
-        nums: &[i32],
-        position: usize
-    ) -> () {
-        if (position >= temp.len()) {
-            result.push(temp.clone());
-            return;
+        fn helper(nums: &[i32], answer: &mut Vec<Vec<i32>>, temp: &mut Vec<i32>, start: usize) {
+            answer.push(temp.clone());
+            for i in start..nums.len() {
+                temp.push(nums[i]);
+                helper(nums, answer, temp, i + 1);
+                temp.pop();
+            }
         }
-        temp.push(nums[position]);
-        Solution::backtrack(result, temp, nums, position + 1);
-        temp.pop();
-        Solution::backtrack(result, temp, nums, position + 1);
+        let mut answer = vec![];
+        helper(&nums[..], &mut answer, &mut vec![], 0);
+        answer
     }
 }
