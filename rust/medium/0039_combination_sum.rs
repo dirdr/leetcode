cfb6e83cd0d886fdbabd5abdbe75de5355e784c3
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2024-05-07 23:06:19 +0200

    Update 0039_combination_sum.rs

--- rust/medium/0039_combination_sum.rs
+++ rust/medium/0039_combination_sum.rs
@@ -1,33 +1,29 @@
+use std::collections::HashSet;
+
 impl Solution {
     pub fn combination_sum(candidates: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
-        let mut result: Vec<Vec<i32>> = vec![];
-        let mut used: Vec<Vec<i32>> = vec![];
-        Self::dfs(&candidates, target, &mut result, &mut vec![], &mut used);
-        result
-    }
-
-    pub fn dfs(
-        candidates: &Vec<i32>,
-        target: i32,
-        result: &mut Vec<Vec<i32>>,
-        path: &mut Vec<i32>,
-        used: &mut Vec<Vec<i32>>,
-    ) -> () {
-        let sum: i32 = path.iter().sum();
-        if (sum > target) {return}
-        if (sum == target) {
-            let mut temp = path.clone();
-            temp.sort();
-            if (!used.contains(&temp)) {
-                used.push(temp);
-                result.push(path.clone());
+        fn backtrack(
+            candidates: &Vec<i32>,
+            target: i32,
+            answer: &mut Vec<Vec<i32>>,
+            current: &mut Vec<i32>,
+            start: usize,
+        ) {
+            if target == 0 {
+                answer.push(current.clone());
                 return;
-            } 
+            }
+            if target < 0 {
+                return;
+            }
+            for i in start..candidates.len() {
+                current.push(candidates[i]);
+                backtrack(candidates, target - candidates[i], answer, current, i);
+                current.pop();
+            }
         }
-        for i in 0..candidates.len() {
-            path.push(candidates[i]);
-            Self::dfs(candidates, target, result, path, used);
-            path.pop();
-        }
+        let mut answer = vec![];
+        backtrack(&candidates, target, &mut answer, &mut vec![], 0);
+        answer
     }
 }
