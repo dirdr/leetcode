bac2c1e7c95a8196494a00a62beb707e9884415a
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2024-09-12 18:27:33 +0200

    Create 1684_count_the_number_of_consistent_string.rs

---
+++ rust/easy/1684_count_the_number_of_consistent_string.rs
@@ -0,0 +1,8 @@
+impl Solution {
+    pub fn count_consistent_strings(allowed: String, words: Vec<String>) -> i32 {
+        words
+            .iter()
+            .filter(|&word| word.chars().all(|c| allowed.contains(c)))
+            .count() as i32
+    }
+}
