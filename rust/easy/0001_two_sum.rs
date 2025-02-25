4df232ebf4ad0f44b06ade66d32e8fa5f7c739e1
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2025-02-25 12:46:45 +0100

    Update 0001_two_sum.rs

--- rust/easy/0001_two_sum.rs
+++ rust/easy/0001_two_sum.rs
@@ -2,12 +2,14 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map: HashMap<i32, i32> = HashMap::new();
-        for (i, el) in nums.iter().enumerate() {
-            if let Some(idx) = map.get(el) {
-                return vec![*idx, i as i32];
+        let mut map = HashMap::new();
+        for (i, &el) in nums.iter().enumerate() {
+            let needed = target - el;
+            if let Some(&idx) = map.get(&needed) {
+                return vec![i as i32, idx as i32];
+            } else {
+                map.insert(el, i);
             }
-            map.insert(target - *el, i as i32);
         }
         unreachable!()
     }
