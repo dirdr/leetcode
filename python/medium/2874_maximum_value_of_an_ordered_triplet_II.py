75955cd5b1b5a8192dfbea7dfd31fd85dcc9a913
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2025-04-03 09:15:52 +0200

    2874_maximum_value_of_an_ordered_triplet_II.py O(n)TC, O(n)SC

---
+++ python/medium/2874_maximum_value_of_an_ordered_triplet_II.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumTripletValue(self, nums: List[int]) -> int:
+        n = len(nums)
+        prefix, suffix = [0] * n, [0] * n
+        prefix[0] = nums[0]
+        suffix[n - 1] = nums[n - 1]
+
+        for i in range(1, n):
+            prefix[i] = max(prefix[i - 1], nums[i])
+            
+        for i in range(n - 2, 1, -1):
+            suffix[i] = max(suffix[i + 1], nums[i])
+
+        answer = 0
+        for i in range(1, n - 1):
+            triplet = (prefix[i - 1] - nums[i]) * suffix[i + 1]
+            answer = max(answer, triplet)
+
+        return answer
