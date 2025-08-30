9307a70cbb793fe763718dd9eeb5456a74ce6ce2
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2025-08-30 11:56:36 +0200

    Refactor is_valid_sudoku for efficiency and clarity

--- rust/medium/0036_valid_sudoku.rs
+++ rust/medium/0036_valid_sudoku.rs
@@ -1,42 +1,46 @@
-use std::collections::HashSet;
-
 impl Solution {
     pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
-        for row in board.clone() {
-            if Solution::check_if_duplicate(&row) {return false}
+        let (m, n) = (board.len(), board[0].len());
+        for i in 0..m {
+            let mut mask: u16 = 0;
+            for j in 0..n {
+                if let Some(digit) = board[i][j].to_digit(10) {
+                    let bit = 1 << (digit - 1);
+                    if mask & bit != 0 {
+                        return false;
+                    }
+                    mask |= bit;
+                }
+            }
         }
-        let mut transposed: Vec<Vec<char>> = vec![Vec::with_capacity(9); 9];
-        for row in board.clone() {
-            for i in 0..9 {
-                transposed[i].push(row[i]);
-            } 
+        for j in 0..n {
+            let mut mask: u16 = 0;
+            for i in 0..m {
+                if let Some(digit) = board[i][j].to_digit(10) {
+                    let bit = 1 << (digit - 1);
+                    if mask & bit != 0 {
+                        return false;
+                    }
+                    mask |= bit;
+                }
+            }
         }
-        for row in transposed {
-            if Solution::check_if_duplicate(&row) {return false}
-        }
-        let mut row_offset = 0;
-        let mut col_offset = 0;
-        for _ in 0..3 {
-            for _ in 0..3 {
-                let mut temp: Vec<char> = Vec::new();
-                for i in 0..3 {
-                    for j in 0..3 {
-                        temp.push(board[i + row_offset][j + col_offset]);
+        for i in (0..m).step_by(3) {
+            for j in (0..n).step_by(3) {
+                let mut mask: u16 = 0;
+                for di in 0..3 {
+                    for dj in 0..3 {
+                        if let Some(digit) = board[i + di][j + dj].to_digit(10) {
+                            let bit = 1 << (digit - 1);
+                            if mask & bit != 0 {
+                                return false;
+                            }
+                            mask |= bit;
+                        }
                     }
                 }
-                if Solution::check_if_duplicate(&temp) {return false}
-                col_offset += 3;
             }
-            col_offset = 0;
-            row_offset += 3;
         }
         true
-    }
-
-    pub fn check_if_duplicate(vec: &Vec<char>) -> bool { 
-        let mut set: HashSet<char> = HashSet::new();
-        !vec.into_iter()
-            .filter(|e| **e != '.')
-            .all(|e| set.insert(*e))
     }
 }
