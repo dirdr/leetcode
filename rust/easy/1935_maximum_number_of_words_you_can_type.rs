901270b568648424295bbd03f1149e57bd971e0b
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2025-09-15 10:38:16 +0200

    Refactor can_be_typed_words for efficiency

--- rust/easy/1935_maximum_number_of_words_you_can_type.rs
+++ rust/easy/1935_maximum_number_of_words_you_can_type.rs
@@ -1,17 +1,15 @@
 use std::collections::HashSet;
-use std::iter::FromIterator;
 
 impl Solution {
     pub fn can_be_typed_words(text: String, broken_letters: String) -> i32 {
         let mut answer = 0;
-        text.split(' ').for_each(|word| {
-            let mut set: HashSet<char> = HashSet::from_iter(word.chars().clone());
-            let mut flag = true; 
-            broken_letters.chars().clone().for_each(|bl| {
-                if set.contains(&bl) {flag = false}
-            });
-            if flag {answer += 1}
-        });
+        let mut banned = HashSet::<char>::from_iter(broken_letters.chars());
+        for word in text.split(' ') {
+            if worad.chars().any(|c| banned.contains(&c)) {
+                continue
+            }
+            answer += 1;
+        }
         answer
     }
 }
