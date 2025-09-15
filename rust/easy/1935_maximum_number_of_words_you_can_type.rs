6bcc339a02e573e4e08eafaff930690297f12da4
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2025-09-15 10:41:17 +0200

    Update 1935_maximum_number_of_words_you_can_type.rs

--- rust/easy/1935_maximum_number_of_words_you_can_type.rs
+++ rust/easy/1935_maximum_number_of_words_you_can_type.rs
@@ -2,14 +2,9 @@ use std::collections::HashSet;
 
 impl Solution {
     pub fn can_be_typed_words(text: String, broken_letters: String) -> i32 {
-        let mut answer = 0;
         let mut banned = HashSet::<char>::from_iter(broken_letters.chars());
-        for word in text.split(' ') {
-            if worad.chars().any(|c| banned.contains(&c)) {
-                continue
-            }
-            answer += 1;
-        }
-        answer
+        text.split(' ')
+            .filter(|w| w.chars().all(|c| !banned.contains(&c)))
+            .count() as i32
     }
 }
