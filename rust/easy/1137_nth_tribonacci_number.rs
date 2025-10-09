75aed89920b7eb476b84b889c83e11e6c2562d75
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2025-10-09 17:58:59 +0200

    Create 1137_nth_tribonacci_number.rs

---
+++ rust/easy/1137_nth_tribonacci_number.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn tribonacci(n: i32) -> i32 {
+        if n == 0 {
+            return 0;
+        }
+        if n < 3 {
+            return 1;
+        }
+        let mut dp = vec![0; n as usize + 1];
+        dp[1] = 1;
+        dp[2] = 1;
+        for i in 3..dp.len() {
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        }
+        dp[n as usize]
+    }
+}
