23607c5f2a64e01f3b0c31dcd94a7069e3bae413
Adrien PELFRESNE <64414295+dirdr@users.noreply.github.com>
2025-10-20 08:16:58 +0200

    Create 2011_final_value_of_variable_after_performing_operations.rs

---
+++ rust/easy/2011_final_value_of_variable_after_performing_operations.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
+        let mut x = 0;
+        for op in &operations {
+            if op.contains("--") {
+                x -= 1;
+            } else {
+                x += 1;
+            }
+        }
+        x
+    }
+}
