deb8750624c781a5ce9fee629f038dfeab7eabba
Adrien Pelfresne <64414295+dirdr@users.noreply.github.com>
2026-01-24 10:12:06 +0100

    Update 1877_minimize_maximum_pair_sum_in_array.rs

--- rust/medium/1877_minimize_maximum_pair_sum_in_array.rs
+++ rust/medium/1877_minimize_maximum_pair_sum_in_array.rs
@@ -1,18 +1,11 @@
 impl Solution {
-    pub fn min_pair_sum(nums: Vec<i32>) -> i32 {
-        let mut nums = nums.clone();
-        nums.sort();
-        let mut left: usize = 0;
-        let mut right: usize = nums.len() - 1;
-        let mut max: usize = 0;
-        while (left <= right) {
-            let pair_sum = nums[left] as usize + nums[right] as usize;
-            if pair_sum >= max {
-                max = pair_sum;
-            }
-            left += 1;
-            right -= 1;
+    pub fn min_pair_sum(mut nums: Vec<i32>) -> i32 {
+        let n = nums.len();
+        nums.sort_unstable();
+        let mut max = 0;
+        for i in 0..n / 2 {
+            max = max.max(nums[i] + nums[n - 1 - i]);
         }
-        max as i32
+        max
     }
 }
