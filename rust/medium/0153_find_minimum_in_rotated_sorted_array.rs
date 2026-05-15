e0f3b57dd5b22ad2f519dcd8757ed1e305837c4f
Adrien Pelfresne <64414295+dirdr@users.noreply.github.com>
2026-05-15 11:41:52 +0200

    Update 0153_find_minimum_in_rotated_sorted_array.rs

--- rust/medium/0153_find_minimum_in_rotated_sorted_array.rs
+++ rust/medium/0153_find_minimum_in_rotated_sorted_array.rs
@@ -1,22 +1,14 @@
 impl Solution {
     pub fn find_min(nums: Vec<i32>) -> i32 {
-        let (mut left, mut right) = (0 as usize, nums.len() - 1);
-        if nums[right] > nums[left] {
-            return nums[left];
-        }
-        let mut min = nums[left];
-        while left <= right {
-            if nums[left] < nums[right] {
-                min = std::cmp::min(nums[left], min);
-            }
-            let middle = left + (right - left) / 2;
-            min = std::cmp::min(nums[middle], min);
-            if nums[middle] >= nums[left] {
-                left = middle + 1;
+        let (mut l, mut r) = (0, nums.len() - 1);
+        while l < r {
+            let mid = l + (r - l) / 2;
+            if nums[mid] > nums[r] {
+                l = mid + 1;
             } else {
-                right = middle - 1;
+                r = mid;
             }
         }
-        min
+        nums[l].min(nums[0])
     }
 }
